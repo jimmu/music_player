2c106cf8b254139ec111e9ad8eac66804cd5fc11
jimmu <jhunter.mail+git@gmail.com>
2016-03-27 18:05:56 +0100

    Add play capability to gui

--- webServer/src/main/resources/player.js
+++ webServer/src/main/resources/player.js
@@ -2,18 +2,38 @@ function hello(url, thingToSelect) {
   d3.json(url, function(error, json){
     if (error) return console.warn(error);
 
+    var thisRow =
     d3.select(thingToSelect)
     .selectAll("div").data(json)
       .enter()
       .append("div")
       .classed("file", function(d){return d.isLeaf})
-      .classed("directory", function(d){return !d.isLeaf})
-      .on("click", (function(d){
+      .classed("directory", function(d){return !d.isLeaf});
+
+    thisRow.on("click", (function(d){
         d3.event.stopPropagation();
-        hello(d.listActionUrl, this);
-      }))
-      .text(function(d){
+        if (d.listActionUrl){
+          hello(d.listActionUrl, this);
+        }
+      }));
+
+    thisRow.append("span").text(function(d){
         return d.name;
-      })
+      });
+
+    thisRow.append("span").text(" - play")
+      .on("click", (function(d){
+           d3.event.stopPropagation();
+           if (d.playActionUrl){
+             play(d.playActionUrl);
+           }
+         }));
   });
+}
+
+function play(url){
+  d3.json(url, function(error, json){
+    if (error) return console.warn(error);
+    console.log(json);
+    });
 }
