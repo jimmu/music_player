b366103ccd58c2df6bcb55e9ce562b25b38f326d
jimmu <jhunter.mail@gmail.com>
2016-11-14 09:58:01 +0000

    Add rest endpoint for simple push service

--- webServer/src/main/java/com/bobbins/rest/Player.java
+++ webServer/src/main/java/com/bobbins/rest/Player.java
@@ -1,11 +1,16 @@
 package com.bobbins.rest;
 
+import java.io.IOException;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 import com.bobbins.PlayerException;
 import com.bobbins.PlayerFactory;
 import com.bobbins.model.PlayingStatusBean;
+import org.glassfish.jersey.media.sse.EventOutput;
+import org.glassfish.jersey.media.sse.OutboundEvent;
+import org.glassfish.jersey.media.sse.SseFeature;
 
 @Path("play")
 public class Player {
@@ -87,4 +92,38 @@ public class Player {
         return null; //TODO. Return a 500 series error
     }
 
+    @GET
+    @Produces(SseFeature.SERVER_SENT_EVENTS)
+    @Path("events")
+    public EventOutput getServerSentEvents(){
+	final EventOutput eventOutput = new EventOutput();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < 10; i++) {
+                        try { Thread.sleep(5000); } catch(InterruptedException e){}
+                        final OutboundEvent.Builder eventBuilder
+                        = new OutboundEvent.Builder();
+                        eventBuilder.name("message-to-client");
+                        eventBuilder.data(String.class,
+                            "Hello world " + i + "!");
+                        final OutboundEvent event = eventBuilder.build();
+                        eventOutput.write(event);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(
+                        "Error when writing the event.", e);
+                } finally {
+                    try {
+                        eventOutput.close();
+                    } catch (IOException ioClose) {
+                        throw new RuntimeException(
+                            "Error when closing the event output.", ioClose);
+                    }
+                }
+            }
+        }).start();
+        return eventOutput;
+    }
 }
