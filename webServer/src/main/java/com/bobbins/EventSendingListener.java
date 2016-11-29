45d1ea8e2ce891a9876e4e4a122567203b25e164
jimmu <jhunter.mail+git@gmail.com>
2016-11-29 10:02:17 +0000

    throttle the number of events sent to the client

--- webServer/src/main/java/com/bobbins/EventSendingListener.java
+++ webServer/src/main/java/com/bobbins/EventSendingListener.java
@@ -8,36 +8,52 @@ import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 
 public class EventSendingListener implements PlayerListener {
-    private EventOutput eventOutput;
 
-    public EventSendingListener(EventOutput eventOutput){
-        this.eventOutput = eventOutput;
+    private static final int THROTTLE_LAG = 100;    //Don't ping the clients less than this many ms apart
+    private PlayingStatusBean latestState;
+    private boolean somethingToSend;
+    private boolean stop;
+
+    public EventSendingListener(final EventOutput eventOutput){
+        somethingToSend = false;
+        stop = false;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!stop){
+                    if (somethingToSend){
+                        try {
+                            synchronized (this) {
+                                final OutboundEvent.Builder eventBuilder = new OutboundEvent.Builder();
+                                eventBuilder.name("player-state-change");
+                                eventBuilder.mediaType(MediaType.APPLICATION_JSON_TYPE);
+                                eventBuilder.data(PlayingStatusBean.class, latestState);
+                                final OutboundEvent event = eventBuilder.build();
+                                if (eventOutput.isClosed()) {    //TODO. Work out if/when we should close this.
+                                    System.out.println("Oh - the EventOutput object is closed!");
+                                    System.out.println("*NOT* writing: " + event);
+                                } else {
+                                    eventOutput.write(event);
+                                    somethingToSend = false;
+                                }
+                            }
+                        } catch (IOException e) {
+                            throw new RuntimeException(
+                                    "Error when writing the event.", e);
+                        }
+                    }
+                    try {
+                        Thread.sleep(THROTTLE_LAG);
+                    } catch (InterruptedException e) {
+                        //ok
+                    }
+                }
+            }
+        }).start();
     }
 
-    public void onChange(PlayingStatusBean state){
-        try {
-            final OutboundEvent.Builder eventBuilder = new OutboundEvent.Builder();
-            eventBuilder.name("player-state-change");
-            eventBuilder.mediaType(MediaType.APPLICATION_JSON_TYPE);
-            eventBuilder.data(PlayingStatusBean.class, state);
-            final OutboundEvent event = eventBuilder.build();
-            if (eventOutput.isClosed()){    //TODO. Work out if/when we should close this.
-                System.out.println("Oh - the EventOutput object is closed!");
-                System.out.println("*NOT* writing: "+event);
-            }
-            else {
-                eventOutput.write(event);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(
-                    "Error when writing the event.", e);
-        } //finally {
-        //try {
-        //eventOutput.close();
-        //} catch (IOException ioClose) {
-        //throw new RuntimeException(
-        //"Error when closing the event output.", ioClose);
-        //}
-        //}
+    public synchronized void onChange(PlayingStatusBean state){
+        latestState = state;
+        somethingToSend = true;
     }
 }
