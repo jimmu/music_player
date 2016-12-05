e6f38580f564e616c558ebe8e9ed954fb43a9b63
jimmu <jhunter.mail@gmail.com>
2016-12-05 17:00:15 +0000

    Change base url. Don't start http server during unit tests

--- webServer/src/main/java/com/bobbins/Main.java
+++ webServer/src/main/java/com/bobbins/Main.java
@@ -18,9 +18,7 @@ import javax.ws.rs.ext.ContextResolver;
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    //public static final String BASE_URI = "http://localhost:8080/myapp/";
-    public static final String BASE_URI = "http://0.0.0.0:8080/myapp/";
-    //public static final String BASE_URI = "http://10.0.2.15:8080/myapp/";
+    public static final String BASE_URI = "http://0.0.0.0:8080/music/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
@@ -55,9 +53,7 @@ public class Main {
      */
     public static void main(String[] args) throws IOException {
         final HttpServer server = startServer();
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        //System.in.read();
+        System.out.println(String.format("Jersey app started with WADL available at " + "%sapplication.wadl", BASE_URI));
 	    try {Thread.sleep(1000*60*60*4);}catch(InterruptedException e){}
         server.stop();
     }
